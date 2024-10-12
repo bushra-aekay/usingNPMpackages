327eaeb0ea3a05d0af4ec024164767ee5fbb4fc5
Bushra Abdul Khader <92054620+bushra-aekay@users.noreply.github.com>
2024-10-12 20:57:50 +0530

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,23 @@
+// // var superheroes = require("superheroes");
+
+// import superheroes from 'superheroes';
+// console.log(superheroes);
+// import {randomSuperhero} from "superheroes";
+
+// var superHeroName = randomSuperhero();
+
+// console.log(superHeroName);
+
+// const superheroes = require('superheroes');
+ 
+// console.log(superheroes.all);
+// //=> ['3-D Man', 'A-Bomb', …]
+ 
+// console.log(superheroes.random());
+// //=> 'Spider-Ham'
+
+import { randomSuperhero } from 'superheroes'; 
+ 
+var randSup = randomSuperhero();
+
+console.log(randSup);
